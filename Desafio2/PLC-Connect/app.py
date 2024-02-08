61d76f2856d69f74c8daa71506c070cecabb403d
Jopei <joaomoura.269@gmail.com>
2024-02-08 13:55:22 -0300

    Cmunicação com o PLC finalizada

--- Desafio2/PLC-Connect/app.py
+++ Desafio2/PLC-Connect/app.py
@@ -2,6 +2,14 @@ from flask import Flask, render_template
 import pyrebase
 import time
 import snap7.client as client
+import snap7.client as c
+from snap7.util import *
+from snap7.snap7types import *
+import numbers
+import time
+import sys
+import os
+import snap7
 
 app = Flask(__name__)
 # Configure sua conexão com o Firebase
@@ -21,19 +29,18 @@ firebase = pyrebase.initialize_app(config)
 # Referência para o Firebase Realtime Database
 db = firebase.database()
 
+TestePLC = c.Client()
+TestePLC.connect("192.168.1.10",0,1)
 # Função para buscar o dado booleano e armazená-lo em uma variável
-# Rota para a página inicial
 @app.route('/')
 def index():
     # Busca o dado booleano no Firebase
     dado_booleano = buscar_dado_booleano("ligado")
     
     # Envia para o PLC
-    if dado_booleano is not None:
-        enviar_para_plc(dado_booleano)
+    
     
     return render_template('index.html', dado_booleano=dado_booleano)
-
 # Função para buscar o dado booleano
 def buscar_dado_booleano(caminho):
     try:
@@ -49,6 +56,7 @@ def get_dado_booleano():
     # Suponha que dado_booleano seja uma variável global
     # ou definida em um escopo acessível aqui
     dado_booleano = buscar_dado_booleano("ligado")# Exemplo: substitua pelo seu valor real
+    EscreveDados(TestePLC,0,S7WLBit,1,0,dado_booleano)
     return str(dado_booleano)
 
 def enviar_segundo_para_firebase():
@@ -73,6 +81,35 @@ def enviar_para_plc(valor):
         plc.disconnect()
     except Exception as e:
         print("Ocorreu um erro ao enviar dados para o PLC:", e)
+
+def ReadMemory(plc,byte,datatype,db,bit=0,tam_st=0):
+    if datatype=='String':
+        result = plc.read_area(areas['DB'],db,byte,tam_st+2)
+        return get_string(result,0,tam_st+2)
+    else:
+        result = plc.read_area(areas['DB'],db,byte,datatype)
+    if datatype==S7WLBit:
+        return get_bool(result,0,bit)
+    elif datatype==S7WLByte or datatype==S7WLWord:
+        return get_int(result,0)
+    elif datatype==S7WLReal:
+        return get_real(result,0)
+    elif datatype==S7WLDWord:
+        return get_dword(result,0)
+    else:
+        return None
+
+def EscreveDados(plc,byte,datatype,db,bit,valor):
+    resultado = plc.read_area(areas['DB'],db,byte,datatype)
+    if datatype==S7WLBit:
+        set_bool(resultado,0,bit,valor)
+    elif datatype==S7WLByte or datatype==S7WLWord:
+        set_int(resultado,0,valor)
+    elif datatype==S7WLReal:
+        set_real(resultado,0,valor)
+    elif datatype==S7WLDWord:
+        set_dword(resultado,0,valor)
+    plc.write_area(areas["DB"],db,byte,resultado)
 
 if __name__ == '__main__':
     # Inicia uma thread separada para enviar o tempo para o Firebase continuamente
